14899e056071843f07555827f2789f12d973c1e8
Albertus Kevin Tandiono <kevintand2609@gmail.com>
2025-02-25 13:54:56 +0700

    masih bingung

--- main/main.cpp
+++ main/main.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
-string adtg (){
-    string tg;
-    cout << "Masukan tugas: ";
-    cin << tg
+string tugas [20] = {};
 
+void adtg (){
+    int n;
+    cout << "Masukan id tugas: ";
+    cin >> n;
+    cout << "Masukan Judul Tugas: ";
+    cin >> tugas[n];
+}
+
+void deltg(){
+    int n;
+    cout<< "Masukan ID tugas yang akan di hapus: ";
+    cin >> n;
+    
 }
 
 int main (){
